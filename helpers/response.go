5b10238c3d92ad733b597db3b1038231365cfb8a
tungdevpro <tungdo21899@gmail.com>
2023-08-30 22:48:59 +0700

    hotfix* custom success response

--- helpers/response.go
+++ helpers/response.go
@@ -4,6 +4,22 @@ type Response struct {
 	StatusCode int         `bson:"code" json:"code"`
 	Message    string      `bson:"message" json:"message"`
 	Data       interface{} `bson:"data" json:"data"`
+	Paging interface{} `bson:"paging" json:"paging,omitempty"`
+	Filter interface{} `bson:"filter" json:"filter,omitempty"`
+}
+
+func SimpleSuccessRes(data interface{}) *Response {
+	return NewSuccessResponse(data, nil, nil)
+}
+
+func NewSuccessResponse(data,paging, filter  interface{}) *Response  {
+	return &Response{
+		StatusCode: 1,
+		Message: "Success",
+		Data: data,
+		Paging: paging,
+		Filter: filter,
+	}
 }
 
 type ErrorResponse struct {
